f815f651e01d2bda9c6d6fa68b59fc8635314d6d
Laura Lima <laura_limadias@hotmail.com>
2022-08-15 14:12:00 -0300

    Atualizacao API
    Atualizacao dos endpoint /update e /delete

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 import mysql.connector
 from flask import Flask, jsonify
-
+from flask import request
 
 con = mysql.connector.connect(host='localhost',
                       port=3306,
@@ -35,5 +35,21 @@ def listamensagens():
     resposta_lista_mensagens = res
     return jsonify(resposta_lista_mensagens)
 
+@app.route('/update', methods=['GET'])
+def delete():
+    status_old = request.args.get('statusold')
+    status_new = request.args.get('statusnew')
+    sql = f'UPDATE Conversa SET status = {status_new} WHERE status = {status_old};'
+    cursor.execute(sql)
+    con.commit()
+    return "UPDATE STATUS"
+
+@app.route('/delete', methods=['GET'])
+def delete():
+    status = request.args.get('status')
+    sql = f'DELETE FROM Conversa WHERE status = {status};'
+    cursor.execute(sql)
+    con.commit()
+    return "DELETE"
 
 app.run()
