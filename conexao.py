ac395455a923d16020e975e78948d572935d569e
Laura Lima <laura_limadias@hotmail.com>
2022-08-15 13:28:31 -0300

    Atualizacao conexao

--- conexao.py
+++ conexao.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import mysql.connector
-
-
-# In[2]:
-
 
 con = mysql.connector.connect(host='localhost',
                       port=3306,
@@ -16,22 +9,7 @@ con = mysql.connector.connect(host='localhost',
                       password='@User1234',
                       database='Smart_Care')
 
-
-# In[3]:
-
-
 cursor = con.cursor()
-
-
-# In[4]:
-
 
 cursor.close()
 con.close()
-
-
-# In[ ]:
-
-
-
-
