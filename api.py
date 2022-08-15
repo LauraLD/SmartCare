88adbbde227f9ddf28a34aae63bb90530bf01fa4
Laura Lima <laura_limadias@hotmail.com>
2022-08-15 16:02:35 -0300

    Atualizacao API
    Fechando cursor

--- api.py
+++ api.py
@@ -11,13 +11,6 @@ con = mysql.connector.connect(host='localhost',
                       password='@User1234',
                       database='Smart_Care')
 
-
-cursor = con.cursor()
-
-sql = 'SELECT mensagem FROM Conversa;'
-cursor.execute(sql)
-res = cursor.fetchall() #ler o bd
-
 app = Flask(__name__)
 
 app.config['JSON_AS_ASCII'] = False
@@ -30,37 +23,48 @@ def homepage():
 #CREATE
 @app.route('/create', methods=['GET'])
 def create():
+    cursor = con.cursor()
     data = request.args.get('data')
     status = request.args.get('status')
     mensagem = request.args.get('mensagem')
     sql = sql = f'INSERT INTO Conversa (data, status, mensagem) VALUES (STR_TO_DATE("{data}", "%Y-%m-%d"), {status}, "{mensagem}");'
     cursor.execute(sql)
     con.commit()
+    cursor.close()
     return "CREATE"
 
 #READ
 @app.route('/listamensagens')
 def listamensagens():
+    cursor = con.cursor()
+    sql = 'SELECT mensagem FROM Conversa;'
+    cursor.execute(sql)
+    res = cursor.fetchall() #ler o bd
     resposta_lista_mensagens = res
+    cursor.close()
     return jsonify(resposta_lista_mensagens)
 
 #UPDATE
 @app.route('/update', methods=['GET'])
 def update():
+    cursor = con.cursor()
     status_old = request.args.get('statusold')
     status_new = request.args.get('statusnew')
     sql = f'UPDATE Conversa SET status = {status_new} WHERE status = {status_old};'
     cursor.execute(sql)
     con.commit()
+    cursor.close()
     return "UPDATE STATUS"
 
 #DELETE
 @app.route('/delete', methods=['GET'])
 def delete():
+    cursor = con.cursor()
     status = request.args.get('status')
     sql = f'DELETE FROM Conversa WHERE status = {status};'
     cursor.execute(sql)
     con.commit()
+    cursor.close()
     return "DELETE"
 
 app.run()
