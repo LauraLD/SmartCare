ead6320bcec294c08039a365587d54adadc6b081
Laura Lima <laura_limadias@hotmail.com>
2022-08-15 13:23:37 -0300

    Atualizacao API
    Atualizacao do endpoint /listamensagens

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 import mysql.connector
 from flask import Flask, jsonify
 
@@ -15,41 +13,12 @@ con = mysql.connector.connect(host='localhost',
 
 cursor = con.cursor()
 
-#CREATE
-
-#data = "2020-01-01"
-#status = 5
-#mensagem = "Mensagem Teste Laura"
-#sql = f'INSERT INTO Conversa (data, status, mensagem) VALUES (STR_TO_DATE("{data}", "%Y-%m-%d"), {status}, "{mensagem}");'
-#cursor.execute(sql)
-#con.commit() #editar o bd
-
-
 #READ
 
 sql = 'SELECT mensagem FROM Conversa;'
 cursor.execute(sql)
 res = cursor.fetchall() #ler o bd
 print(res)
-
-
-
-#UPDATE
-
-#status = 4
-#mensagem = "Mensagem Teste Laura"
-#sql = f'UPDATE Conversa SET status = {status} WHERE mensagem = "{mensagem}";'
-#cursor.execute(sql)
-#con.commit() #editar o bd
-
-
-#DELETE
-
-#mensagem = "Mensagem Teste Laura"
-#sql = f'DELETE FROM Conversa WHERE mensagem = "{mensagem}";'
-#cursor.execute(sql)
-#con.commit() #editar o bd
-
 
 app = Flask(__name__)
 
@@ -63,16 +32,8 @@ def homepage():
 
 @app.route('/listamensagens')
 def listamensagens():
-    #tabela = res
-    #total_pendente = (tabela['Status'] == 'Pendente').sum()
-    #resposta_total_pendente = {'total_pendente':int(total_pendente)}
     resposta_lista_mensagens = res
     return jsonify(resposta_lista_mensagens)
 
 
 app.run()
-
-
-
-
-
